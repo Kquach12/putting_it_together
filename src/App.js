bf03c511613400a3de056a1c03b1a40f22a9724e
Kenny Quach <kennyquach60@gmail.com>
2021-12-20 15:19:45 -0800

    Create people using class components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PersonCard from "./components/PersonCard";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard
+        lastName={"Robinson"}
+        firstName={"Joel"}
+        age={53}
+        hair={"dark brown"}
+      />
+      <PersonCard
+        lastName={"Robot"}
+        firstName={"Crow T."}
+        age={31}
+        hair={"none"}
+      />
+      <PersonCard lastName={"Servo"} firstName={"Tom"} age={31} hair={"none"} />
+      <PersonCard
+        lastName={"Frank"}
+        firstName={"TV's"}
+        age={55}
+        hair={"white"}
+      />
     </div>
   );
 }
